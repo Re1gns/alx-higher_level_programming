6b1803ffc6780d9411f746875f13efc7356167ac
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-06-29 11:41:07 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,10 +6,24 @@
 """
 import sys
 import urllib.request
+from urllib.error import HTTPError
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please provide a URL.")
+        sys.exit(1)
+
     url = sys.argv[1]
 
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            print(dict(response.headers).get("X-Request-Id"))
+
+    except HTTPError as e:
+        print(f"HTTP Error: {e.code} {e.reason}")
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+        sys.exit(1)
