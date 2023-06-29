459d0d16fa4466f96585f7da9ad9102aa2955c89
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-06-29 10:35:59 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,19 @@
 """fetches https://intranet.hbtn.io/status."""
 import requests
 
+# URL to fetch
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+# Send the request and fetch the response
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Get the response body
+    body = response.text
+
+    # Display the response body with tabulation
+    print("- Response body -")
+    print(body)
+else:
+    print("Request failed with status code:", response.status_code)
