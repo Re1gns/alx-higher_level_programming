e525c1e88ceda4f7e61cd204fce9d28883562bfd
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-06-29 11:34:57 +0100

    python network 1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,7 +2,7 @@
 """A script that:
 - takes in a URL,
 - sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header of the response.
+- of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
 import urllib.request
