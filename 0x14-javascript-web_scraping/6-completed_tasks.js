5ea66eb3c4518524c8d2f885a6cebf8fc17251e9
Reigns <117748085+Re1gns@users.noreply.github.com>
2023-07-25 14:32:07 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,32 +1,26 @@
-#!/bin/bash
+#!/usr/bin/node
 
-# Check if the URL argument is provided
-if [ -z "$1" ]; then
-  echo "Please provide the API URL as an argument."
-  exit 1
-fi
+const request = require('request');
+const url = process.argv[2];
 
-# Make the request and process the response
-request_output=$(curl -s "$1")
-completed_users=()
-
-# Loop through the tasks
-while IFS= read -r task; do
-  completed=$(echo "$task" | jq -r '.completed')
-  if [ "$completed" = "true" ]; then
-    userId=$(echo "$task" | jq -r '.userId')
-    completed_users+=("$userId")
-  fi
-done <<< "$request_output"
-
-# Calculate the number of completed tasks by user id
-declare -A completed_counts
-for user_id in "${completed_users[@]}"; do
-  ((completed_counts["$user_id"]++))
-done
-
-# Print users with completed tasks
-for user_id in "${!completed_counts[@]}"; do
-  completed_count=${completed_counts["$user_id"]}
-  echo "User $user_id completed $completed_count tasks."
-done
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});
